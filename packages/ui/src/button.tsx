1bab6f3b1bb29e606dacb5b7b8271cebd37f20de
sumitnair26 <sumit.nair26@gmail.com>
2024-03-16 20:09:55 +0530

    turbo change

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -14,7 +14,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
-      {children}
+      Button will change in both Repo
     </button>
   );
 };
